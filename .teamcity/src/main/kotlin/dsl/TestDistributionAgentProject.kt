9b135c29461c502b1051b4cc98e5cacc4929e429
Bo Zhang <bo@gradle.com>
2025-10-23 15:08:40 +0800

    cleanup

--- .teamcity/src/main/kotlin/dsl/TestDistributionAgentProject.kt
+++ .teamcity/src/main/kotlin/dsl/TestDistributionAgentProject.kt
@@ -4,13 +4,4 @@ import jetbrains.buildServer.configs.kotlin.Project
 
 
 object TestDistributionAgentProject : Project({
-    buildType(Verify("ubuntu", "Default"))
-    params {
-        param("JdkProviderEnabled", "true")
-        param(
-            "AwsEnvironmentVariables", "-e AWS_DEFAULT_REGION=eu-central-1 " +
-                "-e AWS_ACCESS_KEY_ID=%gradle.eng.tooling.aws.access.key.id% " +
-                "-e AWS_SECRET_ACCESS_KEY=%gradle.eng.tooling.aws.secret.access.key%"
-        )
-    }
 })

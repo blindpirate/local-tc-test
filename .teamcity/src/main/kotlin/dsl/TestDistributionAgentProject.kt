e6c974bd20f5563532c07bf5e994fff3d12240f5
Bo Zhang <bo@gradle.com>
2025-10-23 15:16:57 +0800

    test

--- .teamcity/src/main/kotlin/dsl/TestDistributionAgentProject.kt
+++ .teamcity/src/main/kotlin/dsl/TestDistributionAgentProject.kt
@@ -1,7 +1,29 @@
 package dsl
 
+import jetbrains.buildServer.configs.kotlin.BuildType
 import jetbrains.buildServer.configs.kotlin.Project
+import jetbrains.buildServer.configs.kotlin.buildSteps.script
 
+
+object Build : BuildType({
+    name = "MyBuild"
+    features {
+        feature {
+            type = "aws-secrets-build-feature"
+        }
+    }
+
+    steps {
+        script {
+            name = "Test"
+            scriptContent = """
+                echo hello
+            """.trimIndent()
+        }
+    }
+
+})
 
 object TestDistributionAgentProject : Project({
+    buildType(Build)
 })
